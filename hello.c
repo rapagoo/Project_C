8c0dd61534e1bc1649b3bdd79bf8516d451a18b7
Sungrok Hong <lilspicypepper@gmail.com>
2023-03-09 15:22:58 +0900

    laptop upload test!!

--- hello.c
+++ hello.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 int main() {
-	printf("Hello, World! \n");
-	printf("Hello, World! \n");
+	printf("Hello, World!! \n");
+	printf("Upload test!! \n");
+	//Upload Testing...
 	return 0;
 }
